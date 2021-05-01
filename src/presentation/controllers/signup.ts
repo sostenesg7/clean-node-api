f5d1f1477677d33f0de9cbe54b5898652894c0f9
sostenesg7 <sostenesg7@gmail.com>
2021-05-01 00:21:09 -0300

    refactor: ensure SignUpController.handle implement corrent interfaces

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,15 +1,24 @@
+import { HttpRequest, HttpResponse } from '../protocols/http'
+
 export class SignUpController {
-  handle (httpRequest: any): any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
         body: new Error('Missing param: name')
       }
-    } else if (!httpRequest.body.email) {
+    }
+
+    if (!httpRequest.body.email) {
       return {
         statusCode: 400,
         body: new Error('Missing param: email')
       }
+    }
+
+    return {
+      statusCode: 500,
+      body: new Error('Internal server error')
     }
   }
 }
